1cb1cde45add71ac25acc867c47c55ce04bda437
corlard3y <corlardey@gmail.com>
2025-01-11 22:48:54 +0100

    initiate gh-pages

--- src/context/socketContext.ts
+++
@@ -1,6 +0,0 @@
-// React + Web3 Essentials
-import { createContext } from 'react';
-
-const SocketContext = createContext({});
-
-export default SocketContext;
--- src/context/web3context.ts
+++
@@ -1,6 +0,0 @@
-// React + Web3 Essentials
-import { createContext } from 'react';
-
-const Web3Context = createContext({});
-
-export default Web3Context;
